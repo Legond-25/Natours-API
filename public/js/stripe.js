3ce1ae973d41c155f0d1d949153403b07841bca7
Atharva2502 <natharvanitin4@gmail.com>
2021-11-16 12:23:01 +0530

    Added

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,13 +9,11 @@ export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
-    console.log(session);
 
     // 2) Create checkout form + chanre credit card
-    const x = await stripe.redirectToCheckout({
+    await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
     });
-    console.log(x);
   } catch (err) {
     console.log(err);
     showAlert('error', err);
